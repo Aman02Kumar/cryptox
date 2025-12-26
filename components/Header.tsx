7f81e3d73dd3d0efedca930d8157fa7f0de8bc8c
AMAN KAUSHIK <amankaushik998@gmail.com>
2025-12-26 20:19:20 +0530

    implement navigation

---
+++ components/Header.tsx
@@ -0,0 +1,28 @@
+'use client';
+import React from 'react'
+import { usePathname } from 'next/navigation';
+import Link from "next/link";
+import Image from "next/image";
+import { cn } from '@/lib/utils';
+
+const Header = () => {
+    const pathname = usePathname();
+  return (
+    <header>
+        <div className = "main-container inner">
+            <Link href="/">
+                <Image src="logo.svg" alt="Logo" width={132} height={40} />
+            </Link>
+            <nav>
+                <Link href ="/"  className={cn('nav-link', {'is-active': pathname === '/','is-home':true})}>Home</Link>
+
+                <p>Search Modal</p>
+
+                <Link href="/coins">All Coins</Link>
+            </nav>
+        </div>
+    </header>
+  )
+}
+
+export default Header
